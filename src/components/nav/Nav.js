39b0ffbc2d6bb43f086f03798245b1cd854857d2
Daniel Trusman <daniel.trusman@kaiostech.com>
2019-06-25 13:59:07 -0400

    Add logout

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
+import { setAuthedUser } from '../../actions/authedUser';
 import './Nav.css';
 
-export default function Navigation({ login = true }) {
+function Navigation({ login = true, dispatch, users, authedUser }) {
+
+    const user = users[authedUser];
+
+    function handleLogout() {
+        dispatch(setAuthedUser(null));
+    }
 
     if (!login) {
         return (
-            <Nav variant="tabs" defaultActiveKey="home">
-                <Nav.Item>
-                    <Nav.Link eventKey="home">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="new-question">New Question</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <Navbar className="main-navbar">
+                <Nav variant="tabs" defaultActiveKey="home" className="logged-nav-bar">
+                    <Nav.Item>
+                        <Nav.Link eventKey="home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="new-question">New Question</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="leader-board">Leader Board</Nav.Link>
+                    </Nav.Item>
+                    <div className="nav-logout">
+                        <div className="text-logout">
+                            <span className="authed-user">Hello, {user.name} </span>
+                        </div>
+                        <img src={require(`../../resources/icons/${user.avatarURL}`)} alt="Avatar" className="avatar" />
+                        <div className="text-logout">
+                            <Link to="/" onClick={handleLogout}>Logout</Link>
+                        </div>
+                    </div>
+                </Nav>
+            </Navbar>
         )
 
     }
@@ -26,5 +49,13 @@ export default function Navigation({ login = true }) {
             </Navbar>
         )
     }
-
 }
+
+function mapStateToProps(state) {
+    return {
+        users: state.users,
+        authedUser: state.authedUser
+    }
+}
+
+export default connect(mapStateToProps)(Navigation)

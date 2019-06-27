b775903487f07aa3cb1ac71c2a8ad256dde25491
Daniel Trusman <daniel.trusman@kaiostech.com>
2019-06-27 09:15:27 -0400

    Create leader board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Card } from 'react-bootstrap';
+
+import './Board.css';
 
 class Board extends Component {
-    render() {
+
+    renderItem = (user) => {
+
+        const answeredQty = Object.keys(user.answers).length;
+        const createdQty = Object.keys(user.questions).length;
+        const score = answeredQty + createdQty;
+
         return (
-            <div>Boardaaa</div>
+            <Card key={user.id} className="board-card">
+                <Card.Body className="board-body">
+                    <div className="board-image">
+                        <img src={require(`../../resources/icons/${user.avatarURL}`)} alt="Avatar" className="question-avatar" />
+                    </div>
+                    <div className="board-content">
+                        <div>
+                            <div className="board-name">{user.name}</div>
+                            <div className="questions-result-container">
+                                <div className="questions-result">
+                                    <div className="question-result-text"><span> Answered questions</span></div>
+                                    <div><span> {answeredQty}</span></div>
+                                </div>
+                            </div>
+                            <div className="questions-result-container">
+                                <div className="questions-result">
+                                    <div className="question-result-text"><span> Created questions</span></div>
+                                    <div><span> {createdQty}</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="score-content">
+                        <Card className="score-container">
+                            <Card.Header className="score-text">Score</Card.Header>
+                            <Card.Body className="score-body">
+                                <div className="score">
+                                    <span>{score}</span>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Card.Body>
+            </Card>
+        )
+    }
+
+    render() {
+        const { users } = this.props;
+
+        return (
+            <div className="board-container">
+                {Object.values(users).map(this.renderItem)}
+            </div>
         )
     }
 }
 
-export default connect()(Board)
+function mapStateToProps(state) {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps)(Board)
